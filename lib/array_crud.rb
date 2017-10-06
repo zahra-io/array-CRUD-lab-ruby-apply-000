0cb71d3021d651ffe3697c1789ba9d8ed9e704ef
Zahra Khan <zarssskhan@gmail.com>
2017-10-06 06:47:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Chocolate", "Peanut Butter", ""
+  ["Chocolate", "Peanut Butter", "Coffee", "Matcha"]
 end
 
 def add_element_to_end_of_array(array, element)
